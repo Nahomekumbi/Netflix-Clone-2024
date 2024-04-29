d141bb20c132ec3b3043b420603d16934c708f0b
Nahomekumbi <nahomikumbi@gmail.com>
2024-04-28 23:31:13 -0400

    Header & footer

--- netflix-clone/src/components/Heade/Heade.js
+++ netflix-clone/src/components/Heade/Heade.js
@@ -4,7 +4,7 @@ import Netflix from "../../assets/images/NetflixLogo.jpg"
 import SearchIcon from "@mui/icons-material/Search";
 import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
 import AccountBoxIcon from "@mui/icons-material/AccountBox";
-import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 
 const Heade = () => {
   return (
